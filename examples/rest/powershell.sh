298ba969d737a91d3695cec55748aa6b13cad5be
Oleg Putseiko <oleg.putseiko@gmail.com>
2026-05-04 18:39:46 +1000

    feat(powershell): make the powershell example more compact

--- examples/rest/powershell.sh
+++ examples/rest/powershell.sh
@@ -1,19 +1,13 @@
-$headers = @{
-    Authorization = "Bearer sk_live_XXXXXXXXX"
-}
+$headers = @{ Authorization = "Bearer sk_live_XXXXXXXXX" }
 
 $body = @{
     template_id = "tg_XXXXXXXXX"
-    data = @{
-        name = "Jane Doe"
-    }
-} 
-
-$jsonBody = $body | ConvertTo-Json -Depth 10
+    data = @{ name = "Jane Doe" }
+} | ConvertTo-Json -Depth 10
 
 Invoke-RestMethod `
     -Uri "https://api.dispersia.io/messages" `
     -Method Post `
     -Headers $headers `
-    -ContentType "application/json; charset=utf-8" `
-    -Body $jsonBody
+    -Body $body `
+    -ContentType "application/json; charset=utf-8"
